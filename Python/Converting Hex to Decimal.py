b7689cd493524f476ea88d2d2da4cd81f765d922
Pixel Panda <maia.n.costa@outlook.com>
2024-11-16 15:57:04 -0700

    Update Converting Hex to Decimal.py

--- Python/Converting Hex to Decimal.py
+++ Python/Converting Hex to Decimal.py
@@ -1,3 +1,5 @@
+#Writing a function that converts Hexadecimal numbers into decimal numbers. Kind of wanted to know how the int(hexNum, 16) function behaves. I actually might expand this later to other bases. 
+
 hexNumbers = {
     '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
     'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
