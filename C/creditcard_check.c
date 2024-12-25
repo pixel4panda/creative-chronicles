3754533a297844f5977db7ad6c7d72d5e753add0
Pixel Panda <maia.n.costa@outlook.com>
2024-12-25 11:54:55 -0700

    Update creditcard_check.c

--- C/creditcard_check.c
+++ C/creditcard_check.c
@@ -1,6 +1,5 @@
-// Write a program that prompts the user for a credit card number and then reports (via printf) whether it is a valid AmericanExpress, MasterCard, or Visa card number, 
-// per the definitions of each’s format herein. So that we can automate some tests of your code, we ask that your program’s last line of output be AMEX\n or MASTERCARD\n 
-// or VISA\n or INVALID\n, nothing more, nothing less. 
+// This was an assignment to write a program that prompts the user for a credit card number and then reports (via printf) whether it is a valid AmericanExpress, MasterCard, or Visa card number, 
+// per the definitions of each’s format herein. To automate code testing the program’s output should be AMEX\n or MASTERCARD\n or VISA\n or INVALID\n.
 
 #include <cs50.h>
 #include <stdio.h>
