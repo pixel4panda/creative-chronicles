a350aeb1b9cdc4714c0463288a715a14b8a62c76
Pixel Panda <maia.n.costa@outlook.com>
2025-01-17 20:29:46 -0700

    Update bubble_sort.c

--- C/Sort/bubble_sort.c
+++ C/Sort/bubble_sort.c
@@ -1,1 +1,28 @@
 // Code in C that sorts an array through bubble sort
+void bubble_sort (int random[])
+{
+    // Create swap counter
+    int swapc;
+    // Create a space to store value swapped
+    int value;
+
+    do
+    {
+        // Set swap counter to 0
+        swapc = 0;
+        // Remember size is reduce by 1 because I am looking at i and i + 1
+        for (int i = 0; i < array_size - 1; i++)
+        {
+            // Compare pairs
+            if (random[i + 1] < random[i])
+            {
+                value = random[i];
+                random[i] = random[i + 1];
+                random [i + 1] = value;
+
+                swapc++;
+            }
+        }
+    }
+    while (swapc != 0);
+}
