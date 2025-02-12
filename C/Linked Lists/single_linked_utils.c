3b01cda69f0b1dada8b74e559267f6fcc8769d06
Pixel Panda <maia.n.costa@outlook.com>
2025-02-12 15:48:03 -0800

    Create single_linked_utils.c

---
+++ C/Linked Lists/single_linked_utils.c
@@ -0,0 +1,53 @@
+// Assuming node structure as
+typedef struct sllist
+{
+	VALUE val;
+	struct sllist *next;
+}
+sllnode;
+
+// Create a node, where a list doesn't yet exist
+sllnode* create(VALUE xx)
+{
+  sllnode *temp = malloc(sizeof(sllnode);
+  if (temp == NULL)
+  {
+	  return 1;
+  }
+
+  temp->val = xx;
+  temp->next = NULL;
+
+  return temp;
+}
+
+// Search through a linked list to find an element
+bool find (sllnode *head, VALUE xx)
+{
+  for (sllnode *traverse = head; traverse != NULL; traverse = traverse->next)
+  {
+	  if (traverse->val == xx)
+	  {
+		  return true;
+	  }
+  }
+return false;
+}
+
+// Insert a node at the beginning of the list
+sllnode* add_to_list(sllnode *head, VALUE xx)
+{
+  sllnode *new = malloc(sizeofsllnode);
+  if (new == NULL)
+  {
+	  return 1
+  }
+
+  new->val = xx;
+  new->next = head;
+
+  head = new;
+
+  return head;
+}
+
